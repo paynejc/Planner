e223296d94f03c708d528058412a3b81604d7737
paynejc <paynejc84@gmail.com>
2022-01-08 15:39:04 -0500

    Created save button function to save to local

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,38 @@
 $(document).ready(function () {
-
     //Set Date using moment
     $("#currentDay").text(moment().format("MMM Do YY"));
 
     function timeClock() {
         var currentHour = moment().hours();
-        $(".time-block").each(function() {
-            var hourBlock = parseInt($(this).attr("id")\
+        $(".time-block").each(function () {
+            var hourBlock = parseInt($(this).attr("id").split("-")[1]);
 
-            //console.log(hourBlock)
-        })
+            console.log(hourBlock)
+            if (hourBlock < currentHour) {
+                $(this).addClass("past")
+            }
+            else if (hourBlock === currentHour) {
+                $(this).addClass("present")
+            }
+            else { $(this).addClass("future") }
+        });
     }
-}
+    timeClock()
+
+    $(".saveBtn").on("click", function () {
+        var saveNote = $(this).prev().val()
+        var hourId = $(this).parent().attr("id")
+        var update = JSON.parse(localStorage.getItem("description"))
+        var timeSlot = {
+
+        }
+
+    })
+
+
+    
+});
+
+//If hourBlock is less than currentHour past use .this $(this).addClass("past")
+//if hourBlock is more than currentHour future use
+//if hourBlock is equal to currentHour Present
