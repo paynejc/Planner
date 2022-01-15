481d9ece6ae440908def007d1881868f90a21471
paynejc <paynejc84@gmail.com>
2022-01-15 10:33:28 -0500

    Updated ReadME

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
 # Planner
-Planner Assignment
-Description
-Screen shot
-Link 
+For the planner assignment the goal is to create an easy to use daily planner
+Past hour shows as grey, present as red, future as green. 
+Input notes and press save to save your agenda
--- assets/script.js
+++ assets/script.js
@@ -23,13 +23,18 @@ $(document).ready(function () {
         var saveNote = $(this).prev().val()
         var hourId = $(this).parent().attr("id")
         localStorage.setItem( hourId, saveNote)
-        //var update = JSON.parse(localStorage.getItem("description"))
-        var timeSlot = {
 
-        }
 
     })
     $("#hour-9 .description").val(localStorage.getItem("hour-9"))
+    $("#hour-10 .description").val(localStorage.getItem("hour-10"))
+    $("#hour-11 .description").val(localStorage.getItem("hour-11"))
+    $("#hour-12 .description").val(localStorage.getItem("hour-12"))
+    $("#hour-13 .description").val(localStorage.getItem("hour-13"))
+    $("#hour-14 .description").val(localStorage.getItem("hour-14"))
+    $("#hour-15 .description").val(localStorage.getItem("hour-15"))
+    $("#hour-16 .description").val(localStorage.getItem("hour-16"))
+    $("#hour-17 .description").val(localStorage.getItem("hour-17"))
 
 
 });
